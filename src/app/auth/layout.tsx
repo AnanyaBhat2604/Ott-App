be82cbe5c6c2d8fa14cca82ae879cad1a37aa017
HemarajKS <hemaraja.shettigar@robosoftin.com>
2024-05-06 17:11:43 +0530

    signup-and-login-UI-->Api call

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -7,11 +7,13 @@ import Snackbar from "@/components/Snackbar/Snackbar";
 import { SnackbarProvider } from "@/contexts/snackbar-context/snackbar-context";
 import { getRoutePermissions } from "@/utils/route-permissions";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const router = useRouter();
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (
@@ -19,14 +21,20 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       !getRoutePermissions(pathname)
     ) {
       router.push(frontendRoutes.LOGIN);
+    } else {
+      setLoading(false);
     }
   }, [pathname, router]);
 
   return (
     <div className="auth-bg center-div">
       <SnackbarProvider>
-        <div className="custom-card  z-[1] relative max-h-[90vh] overflow-y-auto scrollbar-custom">
-          {children}
+        <div
+          className={`custom-card  z-[1] relative max-h-[90vh] overflow-y-auto scrollbar-custom ${
+            loading && "hidden"
+          }`}
+        >
+          {!loading && children}
         </div>
         <Snackbar />
       </SnackbarProvider>
