0f23aff01abda86908a1b911269f68ed323db7b0
HemarajKS <hemaraja.shettigar@robosoftin.com>
2024-05-10 16:52:44 +0530

    removed server

--- server/src/controllers/menuController.mjs
+++
@@ -1,9 +0,0 @@
-import menuJSON from "../../assets/data/menu.json" assert { type: "json" };
-
-export const getMenu = (req, res) => {
-  try {
-    res.json({ data: menuJSON });
-  } catch (error) {
-    res.status(500).json({ status: 500, message: "Internal server error" });
-  }
-};
--- server/src/routes/menuRoutes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getMenu } from "../controllers/menuController.mjs";
-
-const router = express.Router();
-
-router.get("/menu", getMenu);
-
-export default router;
