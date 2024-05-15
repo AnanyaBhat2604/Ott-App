6a9530cecb009932c86ca7a79671b87a4bbf0739
HemarajKS <hemaraja.shettigar@robosoftin.com>
2024-05-15 20:49:02 +0530

    changes

--- src/package/FAQ/FAQ.tsx
+++ src/package/FAQ/FAQ.tsx
@@ -1,10 +1,14 @@
+import Accordion from "@/components/Accordion/Accordion";
 import Title from "@/components/Title/Title";
 import React, { FC } from "react";
 
 const FAQ: FC<{ data: any; title: string }> = ({ data, title }) => {
   return (
-    <div>
+    <div className=" flex flex-col gap-[20px] p-[40px] relative">
       <Title title={title} />
+      <div>
+        <Accordion />
+      </div>
     </div>
   );
 };
