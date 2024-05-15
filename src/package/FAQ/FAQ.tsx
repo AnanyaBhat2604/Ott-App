274b40de9182c1f49c01db929dbf0f53772dadc6
HemarajKS <hemaraja.shettigar@robosoftin.com>
2024-05-15 22:31:50 +0530

    added accordion

--- src/package/FAQ/FAQ.tsx
+++ src/package/FAQ/FAQ.tsx
@@ -1,14 +1,15 @@
-import Accordion from "@/components/Accordion/Accordion";
+import Accordion from "@/components/Accordion";
 import Title from "@/components/Title/Title";
 import React, { FC } from "react";
 
 const FAQ: FC<{ data: any; title: string }> = ({ data, title }) => {
   return (
     <div className=" flex flex-col gap-[20px] p-[40px] relative">
-      <Title title={title} />
-      <div>
-        <Accordion />
+      <div className="text-center">
+        <Title title={title} />
       </div>
+
+      <Accordion data={data.contents} />
     </div>
   );
 };
