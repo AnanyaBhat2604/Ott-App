01e41c2271d5640b1132bf95d3df41ed01cb6cd8
HemarajKS <hemaraja.shettigar@robosoftin.com>
2024-05-16 09:45:39 +0530

    added hero

--- src/services/PackageSelector.ts
+++ src/services/PackageSelector.ts
@@ -1,5 +1,6 @@
 import Rails from "@/package/Rails/Rails";
 import FAQ from "@/package/FAQ/FAQ";
+import Hero from "@/package/Hero/Hero";
 
 function getComponent(key: string) {
   switch (key) {
@@ -7,6 +8,8 @@ function getComponent(key: string) {
       return Rails;
     case "FAQ":
       return FAQ;
+    case "Hero":
+      return Hero;
     default:
       throw new Error(`Unknown component key: ${key}`);
   }
