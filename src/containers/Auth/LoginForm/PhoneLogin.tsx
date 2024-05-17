1abef7d6707ade513d049429d6dc11858714d013
HemarajKS <hemaraja.shettigar@robosoftin.com>
2024-05-17 12:05:01 +0530

    changes

--- src/containers/Auth/LoginForm/PhoneLogin.tsx
+++ src/containers/Auth/LoginForm/PhoneLogin.tsx
@@ -110,12 +110,25 @@ const PhoneLogin: FC = () => {
     }));
   };
 
+  const getUpdatedParams = () => {
+    const url = new URL(document.location.href);
+    const searchParams: { [key: string]: string } = {};
+
+    url.searchParams.forEach((value, key) => {
+      searchParams[key] = value;
+    });
+
+    searchParams["email"] = "true";
+
+    return new URLSearchParams(searchParams).toString();
+  };
+
   return (
     <form onSubmit={onSubmit} className="w-full">
       <div className="flex justify-between gap-3 pt-[40px] opacity-70 items-center w-full">
         <div className="text-sm font-semibold">{strings.phone}</div>
         <Link
-          href={`${frontendRoutes.LOGIN}?email=true`}
+          href={`${frontendRoutes.LOGIN}?${getUpdatedParams()}`}
           className="text-xs font-normal"
         >
           {strings.signInWithEmail}
